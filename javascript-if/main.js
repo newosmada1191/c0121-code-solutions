f9b84d82732e312b6de159a94719f22c2163956c
Owen Adams <owenadams.main@gmail.com>
2021-01-28 12:55:23 -0500

    Removed lines 56-60 and added return false for the code declaration of the function

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -53,11 +53,7 @@ function isOldEnoughToDrive(person) {
 }
 
 function isOldEnoughToDrinkAndDrive(person) {
-  if (person.age >= 16 && person.age >= 21) {
-    return false;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 function categorizeAcidity(pH) {
