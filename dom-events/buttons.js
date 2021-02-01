e2654b3ef745de313c794cc189e4ef73f8f69fe3
Owen Adams <owenadams.main@gmail.com>
2021-02-01 16:33:21 -0500

    Removed $ from variables

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,24 +1,24 @@
 // Click Me! event
 function handleClick(event) {
-  var $buttonClicked = 'button clicked';
-  console.log('$buttonClicked', $buttonClicked);
+  var buttonClicked = 'button clicked';
+  console.log('buttonClicked', buttonClicked);
   console.log('event:', event);
   console.log('event.target:', event.target);
 }
 
-var $clickButton = document.querySelector('.click-button');
-$clickButton.addEventListener('click', handleClick);
+var clickButton = document.querySelector('.click-button');
+clickButton.addEventListener('click', handleClick);
 
 // Hover Me! event
 function handleMouseover(event) {
-  var $buttonHovered = 'button hovered';
-  console.log('$buttonHovered:', $buttonHovered);
+  var buttonHovered = 'button hovered';
+  console.log('buttonHovered:', buttonHovered);
   console.log('event:', event);
   console.log('event.target:', event.target);
 }
 
-var $hoverButton = document.querySelector('.hover-button');
-$hoverButton.addEventListener('mouseover', handleMouseover);
+var hoverButton = document.querySelector('.hover-button');
+hoverButton.addEventListener('mouseover', handleMouseover);
 
 // Double Click Me! event
 function handleDoubleClick(event) {
