368fe88b01e0e6a3fb0ad13617b94db8dde9785a
Owen Adams <owenadams.main@gmail.com>
2021-02-16 16:26:53 -0500

    Completed all tests for JavaScript Constructors.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,15 @@
+var anObject = new Object();
+console.log('value of anObject:', anObject);
+console.log('typeof anObject:', typeof anObject);
+
+var anArray = new Array();
+console.log('value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
+
+var aFunction = new Function();
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction:', typeof aFunction);
+
+var aString = new String();
+console.log('value of aString:', aString);
+console.log('typeof aString:', typeof aString);
