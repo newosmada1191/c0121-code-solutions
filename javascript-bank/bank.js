3906824b36440731ea8a7eb89f6bbbb196122fe8
Owen Adams <owenadams.main@gmail.com>
2021-02-17 14:43:52 -0500

    Finished openAccount function in bank.js

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,19 +1,17 @@
 /* exported Bank */
-// function Bank() {
-//   this.nextAccountNumber = 1;
-//   this.accounts = [];
-// }
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
 
 // Bank.prototype.openAccount = function (holder, balance) {
 //   if (balance > 0) {
 //     var account = new Account(this.nextAccountNumber, holder);
-//     // deposit balance into new Account
 //     var newTransaction = new Transaction("deposit", balance);
-//     Account.transaction.push(newTransaction)
-//     // pushed into new account
+//     account.transactions.push(newTransaction);
 //     this.accounts.push(account);
-//     // increments this.accountNumber
-//     this.accountNumber++;
+//     this.nextAccountNumber++;
+//     return account.number;
 //   } else {
 //     return null;
 //   }
