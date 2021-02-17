53fc6c6ee29a22f65338bb40eaded4aeef760eb2
Owen Adams <owenadams.main@gmail.com>
2021-02-16 22:51:00 -0500

    Bank challenge is a Work in Progress

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,22 @@
 /* exported Bank */
+// function Bank() {
+//   this.nextAccountNumber = 1;
+//   this.accounts = [];
+// }
+
+// Bank.prototype.openAccount = function (holder, balance) {
+//   if (balance > 0) {
+//     var account = new Account(this.nextAccountNumber, holder);
+//     // deposit balance into new Account
+//   } else {
+//     return null;
+//   }
+// }
+
+// Bank.prototype.getAccount = function () {
+
+// }
+
+// Bank.prototype.getTotalAssets = function () {
+
+// }
