8d2d7b168e69cac4b6911fb9865afcec70437594
Owen Adams <owenadams.main@gmail.com>
2021-02-17 12:53:28 -0500

    Minor changes to openAccount function. Work in progress

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -8,6 +8,12 @@
 //   if (balance > 0) {
 //     var account = new Account(this.nextAccountNumber, holder);
 //     // deposit balance into new Account
+//     var newTransaction = new Transaction("deposit", balance);
+//     Account.transaction.push(newTransaction)
+//     // pushed into new account
+//     this.accounts.push(account);
+//     // increments this.accountNumber
+//     this.accountNumber++;
 //   } else {
 //     return null;
 //   }
