86f8212062e5b98ee60238c8b9b309aef92da80e
Owen Adams <owenadams.main@gmail.com>
2021-02-17 19:09:59 -0500

    Added intervals to automatically cycle through images

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -7,31 +7,37 @@ var $circleThree = document.querySelector('#circle-three');
 var $circleFour = document.querySelector('#circle-four');
 var $circleFive = document.querySelector('#circle-five');
 
-$rightArrow.addEventListener('click', function (event) {
-  if ($img.getAttribute('src') === 'images/001.png') {
-    $img.setAttribute('src', 'images/004.png');
-    $circleOne.setAttribute('class', 'far fa-circle');
-    $circleTwo.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/004.png') {
-    $img.setAttribute('src', 'images/007.png');
-    $circleTwo.setAttribute('class', 'far fa-circle');
-    $circleThree.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/007.png') {
-    $img.setAttribute('src', 'images/025.png');
-    $circleThree.setAttribute('class', 'far fa-circle');
-    $circleFour.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/025.png') {
-    $img.setAttribute('src', 'images/039.png');
-    $circleFour.setAttribute('class', 'far fa-circle');
-    $circleFive.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/039.png') {
-    $img.setAttribute('src', 'images/001.png');
-    $circleFive.setAttribute('class', 'far fa-circle');
-    $circleOne.setAttribute('class', 'fas fa-circle');
-  }
-});
+var intervalId = setInterval(rightClick, 5000);
 
-$leftArrow.addEventListener('click', function (event) {
+$rightArrow.addEventListener('click', rightClick);
+
+function rightClick(event) {
+  if ($img.getAttribute('src') === 'images/001.png') {
+    $img.setAttribute('src', 'images/004.png');
+    $circleOne.setAttribute('class', 'far fa-circle');
+    $circleTwo.setAttribute('class', 'fas fa-circle');
+  } else if ($img.getAttribute('src') === 'images/004.png') {
+    $img.setAttribute('src', 'images/007.png');
+    $circleTwo.setAttribute('class', 'far fa-circle');
+    $circleThree.setAttribute('class', 'fas fa-circle');
+  } else if ($img.getAttribute('src') === 'images/007.png') {
+    $img.setAttribute('src', 'images/025.png');
+    $circleThree.setAttribute('class', 'far fa-circle');
+    $circleFour.setAttribute('class', 'fas fa-circle');
+  } else if ($img.getAttribute('src') === 'images/025.png') {
+    $img.setAttribute('src', 'images/039.png');
+    $circleFour.setAttribute('class', 'far fa-circle');
+    $circleFive.setAttribute('class', 'fas fa-circle');
+  } else if ($img.getAttribute('src') === 'images/039.png') {
+    $img.setAttribute('src', 'images/001.png');
+    $circleFive.setAttribute('class', 'far fa-circle');
+    $circleOne.setAttribute('class', 'fas fa-circle');
+  }
+}
+
+$leftArrow.addEventListener('click', leftClick);
+
+function leftClick(event) {
   if ($img.getAttribute('src') === 'images/001.png') {
     $img.setAttribute('src', 'images/039.png');
     $circleOne.setAttribute('class', 'far fa-circle');
@@ -52,5 +58,7 @@ $leftArrow.addEventListener('click', function (event) {
     $img.setAttribute('src', 'images/025.png');
     $circleFive.setAttribute('class', 'far fa-circle');
     $circleFour.setAttribute('class', 'fas fa-circle');
+  } else if (event.target) {
+    clearInterval(intervalId);
   }
-});
+}
