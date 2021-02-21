17de31b98861691390e1bfe485b7f1b13d8ce593
Owen Adams <owenadams.main@gmail.com>
2021-02-20 21:15:05 -0500

    Completed getAccount function of the Bank test

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -4,23 +4,29 @@ function Bank() {
   this.accounts = [];
 }
 
-// Bank.prototype.openAccount = function (holder, balance) {
-//   if (balance > 0) {
-//     var account = new Account(this.nextAccountNumber, holder);
-//     var newTransaction = new Transaction("deposit", balance);
-//     account.transactions.push(newTransaction);
-//     this.accounts.push(account);
-//     this.nextAccountNumber++;
-//     return account.number;
-//   } else {
-//     return null;
-//   }
-// }
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0) {
+    var account = new Account(this.nextAccountNumber, holder);
+    var newTransaction = new Transaction("deposit", balance);
+    account.transactions.push(newTransaction);
+    this.accounts.push(account);
+    this.nextAccountNumber++;
+    return account.number;
+  } else {
+    return null;
+  }
+}
 
-// Bank.prototype.getAccount = function () {
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number)
+    var account = this.accounts[i];
+    return account;
+  } else {
+    return null;
+  }
+}
 
-// }
+Bank.prototype.getTotalAssets = function () {
 
-// Bank.prototype.getTotalAssets = function () {
-
-// }
+}
