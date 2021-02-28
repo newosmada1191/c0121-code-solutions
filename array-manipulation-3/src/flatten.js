01f1470349d4c2972f83d37780916b261f14e6dc
Owen Adams <owenadams.main@gmail.com>
2021-02-27 23:35:41 -0500

    Completed flatten-tests

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,15 @@
 /* exported flatten */
+function flatten(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    var arrayValue = array[i];
+    if (!Array.isArray(arrayValue)) {
+      newArray.push(arrayValue);
+    } else {
+    for (var j = 0; j < arrayValue.length; j++) {
+      newArray.push(arrayValue[j]);
+    }
+    }
+  }
+  return newArray;
+}
