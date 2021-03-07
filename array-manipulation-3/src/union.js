fd14ec3045790b42d85727ace0a9ee3b556f9cb3
Owen Adams <owenadams.main@gmail.com>
2021-03-07 18:22:54 -0500

    completed array-manipulation-3 challenge

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,15 @@
 /* exported union */
+function union(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (newArray.indexOf(first[i]) === -1) {
+      newArray.push(first[i])
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (newArray.indexOf(second[j]) === -1) {
+      newArray.push(second[j])
+    }
+  }
+  return newArray;
+}
