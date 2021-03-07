8c158bd893d0af1a78ce2ae9309d5ec61cf26ce8
Owen Adams <owenadams.main@gmail.com>
2021-03-07 15:36:26 -0500

    Completed intersection tests

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -2,8 +2,10 @@
 function intersection (first, second) {
   var newArray = [];
   for (var i = 0; i < first.length; i++) {
-    if (first[i] === second[i]) {
-      newArray.push(first[i]);
+    for (var j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        newArray.push(first[i]);
+      }
     }
   }
   return newArray;
