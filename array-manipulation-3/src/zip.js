766587d260fc9fa4eb865763199c4631b8b8ee40
Owen Adams <owenadams.main@gmail.com>
2021-03-07 22:54:40 -0500

    updated zip function

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,26 +1,11 @@
 /* exported zip */
 function zip(first, second) {
   var newArray = [];
-  if (first.length < second.length) {
-    for (var i = 0; i < first.length; i++) {
-      var innerArray = [];
-      innerArray.push(first[i], second[i]);
-      newArray.push(innerArray);
-    }
-  }
-  if (first.length > second.length) {
-    for (var j = 0; j < second.length; j++) {
-      var innerArrayTwo = [];
-      innerArrayTwo.push(first[j], second[j]);
-      newArray.push(innerArrayTwo);
-    }
-  }
-  if (first.length === second.length) {
-    for (var k = 0; k < first.length; k++) {
-      var innerArrayThree = [];
-      innerArrayThree.push(first[k], second[k])
-      newArray.push(innerArrayThree);
-    }
+  var range = Math.min(first.length, second.length);
+  for (var i = 0; i < range; i++) {
+    var innerArray = [];
+    innerArray.push(first[i], second[i])
+    newArray.push(innerArray);
   }
   return newArray;
 }
