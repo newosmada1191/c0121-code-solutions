0db550cc6c470f1bcb65343da0f1331075d273cb
Owen Adams <owenadams.main@gmail.com>
2021-03-09 02:20:22 -0500

    difference test work in progress

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,7 +1,18 @@
 /* exported difference */
 function difference (first, second) {
-  return [
-    ...first.filter(x => !second.includes(x)),
-    ...second.filter(x => !first.includes(x))
-  ];
+  var newArray = [];
+  var length = Math.max(first.length, second.length);
+  for (var i = 0; i < length; i++) {
+    if (first[i] !== second[i]) {
+      newArray.push(first[i]);
+      console.log(first[i])
+    }
+  }
+  for (var j = 0; j < length; j++) {
+    if (second[j] !== first[j]) {
+      newArray.push(second[j]);
+      console.log(second[j])
+    }
+  }
+  return newArray;
 }
