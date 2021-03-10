b6d35a1de0c9e39f7a16e008ba643ba7be3770ae
Owen Adams <owenadams.main@gmail.com>
2021-03-10 02:53:35 -0500

    added functionality to event listener, work in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,4 +3,16 @@ var i = 0;
 
 window.addEventListener('keypress', function (event) {
   console.log(event.key);
+  if (event.key === $span[i].textContent) {
+    $span[i].className = 'text correct';
+    i++;
+  } else if (event.key !== $span[i].textContent) {
+    $span[i].className = 'text incorrect';
+  }
+
+  if ($span[i].className === 'text space' && event.key === ' ') {
+    console.log('test');
+    $span[i].className = 'text space correct';
+    i++
+  }
 })
