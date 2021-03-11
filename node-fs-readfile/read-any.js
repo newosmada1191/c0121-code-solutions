73db704905d78df8a24df33245c7dbc9d65a90bb
Owen Adams <owenadams.main@gmail.com>
2021-03-11 12:47:09 -0500

    Removed if conditionals

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -2,20 +2,6 @@ const fs = require('fs');
 
 const textFile = process.argv[2];
 
-if (textFile === 'hopper.txt') {
-  fs.readFile('./hopper.txt', 'utf8', (err, data) => {
+  fs.readFile(textFile, 'utf8', (err, data) => {
     console.log(data);
   })
-} else if (textFile === 'cunningham.txt') {
-  fs.readFile('./cunningham.txt', 'utf8', (err, data) => {
-    console.log(data);
-  })
-} else if (textFile === 'hipster-ipsum.txt') {
-  fs.readFile('./hipster-ipsum.txt', 'utf8', (err, data) => {
-    console.log(data);
-  })
-} else if (textFile === 'dijkstra.txt') {
-  fs.readFile('./djikstra.txt', 'utf8', (err, data) => {
-    console.log(data);
-  })
-}
