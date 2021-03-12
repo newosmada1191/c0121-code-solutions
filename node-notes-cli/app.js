912c40095fdf8433d18ced24b767e4348960819c
Owen Adams <owenadams.main@gmail.com>
2021-03-11 21:22:38 -0500

    user can create and edit a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,15 +1,59 @@
-const fs = require('fs')
+const fs = require('fs');
 
-if (process.argv[2] === 'read') {
+const appCommand = process.argv[2];
+
+if (appCommand === 'read') {
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-    let noteValues = JSON.parse(data)
+    const noteValues = JSON.parse(data);
+    let i = 1;
     for (i in noteValues.notes) {
       console.log(i + ':', noteValues.notes[i]);
-      i++
+      i++;
     }
-  })
+  });
+}
+
+if (appCommand === 'create') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    const userInput = process.argv[3];
+    const parsedData = JSON.parse(data);
+    const nextId = parsedData.nextId;
+    parsedData.notes[nextId] = userInput;
+    parsedData.nextId++;
+    const newText = JSON.stringify(parsedData, null, 2);
+    fs.writeFile('data.json', newText, 'utf8', err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  });
+}
+
+if (appCommand === 'update') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    const parsedData = JSON.parse(data);
+    const id = process.argv[3];
+    const userInput = process.argv[4];
+    parsedData.notes[id] = userInput;
+    const newText = JSON.stringify(parsedData, null, 2);
+    fs.writeFile('data.json', newText, 'utf8', err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  });
 }
