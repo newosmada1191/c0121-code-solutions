539c117688da5af4728c29fee8655e8a69886590
Owen Adams <owenadams.main@gmail.com>
2021-03-14 16:56:16 -0400

    Work in progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,69 +1,51 @@
 var $rightArrow = document.querySelector('#right-arrow-icon');
 var $leftArrow = document.querySelector('#left-arrow-icon');
 var $img = document.querySelector('#image-source');
-var $circleOne = document.querySelector('#circle-one');
-var $circleTwo = document.querySelector('#circle-two');
-var $circleThree = document.querySelector('#circle-three');
-var $circleFour = document.querySelector('#circle-four');
-var $circleFive = document.querySelector('#circle-five');
+var $circleOne = document.querySelector('#circle-1');
+var $circleTwo = document.querySelector('#circle-2');
+var $circleThree = document.querySelector('#circle-3');
+var $circleFour = document.querySelector('#circle-4');
+var $circleFive = document.querySelector('#circle-5');
+const imgArray = [
+  'images/001.png',
+  'images/004.png',
+  'images/007.png',
+  'images/025.png',
+  'images/039.png'
+];
+let currentImg = '';
 
-var intervalID = setInterval(rightClick, 3000);
+$rightArrow.addEventListener('click', () => {
+  currentImg = $img.getAttribute('src');
+  rightClick(currentImg);
+});
 
-$rightArrow.addEventListener('click', rightClick);
-
-function rightClick(event) {
-  if ($img.getAttribute('src') === 'images/001.png') {
-    $img.setAttribute('src', 'images/004.png');
-    $circleOne.setAttribute('class', 'far fa-circle');
-    $circleTwo.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/004.png') {
-    $img.setAttribute('src', 'images/007.png');
-    $circleTwo.setAttribute('class', 'far fa-circle');
-    $circleThree.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/007.png') {
-    $img.setAttribute('src', 'images/025.png');
-    $circleThree.setAttribute('class', 'far fa-circle');
-    $circleFour.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/025.png') {
-    $img.setAttribute('src', 'images/039.png');
-    $circleFour.setAttribute('class', 'far fa-circle');
-    $circleFive.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/039.png') {
-    $img.setAttribute('src', 'images/001.png');
-    $circleFive.setAttribute('class', 'far fa-circle');
-    $circleOne.setAttribute('class', 'fas fa-circle');
+function rightClick(currentImg) {
+  const currentImgIndex = imgArray.indexOf(currentImg);
+  let nextImgIndex;
+  if (currentImgIndex === imgArray.length - 1) {
+    nextImgIndex = 0;
+  } else {
+    nextImgIndex = currentImgIndex + 1;
   }
-  clearInterval(intervalID);
-  intervalID = setInterval(rightClick, 3000);
+  $img.setAttribute('src', imgArray[nextImgIndex]);
+  const $currentCircle = document.querySelector(`#circle-${currentImgIndex + 1}`);
+  const $nextCircle = document.querySelector(`#circle-${nextImgIndex + 1}`);
+  $currentCircle.classList.remove('fas');
+  $currentCircle.classList.add('far');
+  $nextCircle.classList.remove('far');
+  $nextCircle.classList.add('fas');
 }
 
-$leftArrow.addEventListener('click', leftClick);
+var intervalID = setInterval(() => {
+  const currentImg = $img.getAttribute('src');
+  rightClick(currentImg);
+}, 3000);
 
-function leftClick(event) {
-  if ($img.getAttribute('src') === 'images/001.png') {
-    $img.setAttribute('src', 'images/039.png');
-    $circleOne.setAttribute('class', 'far fa-circle');
-    $circleFive.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/004.png') {
-    $img.setAttribute('src', 'images/001.png');
-    $circleTwo.setAttribute('class', 'far fa-circle');
-    $circleOne.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/007.png') {
-    $img.setAttribute('src', 'images/004.png');
-    $circleThree.setAttribute('class', 'far fa-circle');
-    $circleTwo.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/025.png') {
-    $img.setAttribute('src', 'images/007.png');
-    $circleFour.setAttribute('class', 'far fa-circle');
-    $circleThree.setAttribute('class', 'fas fa-circle');
-  } else if ($img.getAttribute('src') === 'images/039.png') {
-    $img.setAttribute('src', 'images/025.png');
-    $circleFive.setAttribute('class', 'far fa-circle');
-    $circleFour.setAttribute('class', 'fas fa-circle');
-  }
-  clearInterval(intervalID);
-  intervalID = setInterval(rightClick, 3000);
-}
+$leftArrow.addEventListener('click', () => {
+  currentImg = $img.getAttribute('src');
+  rightClick(currentImg);
+});
 
 $circleOne.addEventListener('click', function (event) {
   $img.setAttribute('src', 'images/001.png');
