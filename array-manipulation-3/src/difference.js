ec13378cd46ade076dfddf6aecc2c35c230e5f6c
Owen Adams <owenadams.main@gmail.com>
2021-03-15 00:08:04 -0400

    Work in Progress

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,17 +1,14 @@
 /* exported difference */
-function difference (first, second) {
-  var newArray = [];
-  var length = Math.max(first.length, second.length);
-  for (var i = 0; i < length; i++) {
+function difference(first, second) {
+  const newArray = [];
+  for (let i = 0; i < first.length; i++) {
     if (first[i] !== second[i]) {
       newArray.push(first[i]);
-      console.log(first[i])
     }
   }
-  for (var j = 0; j < length; j++) {
+  for (let j = 0; j < second.length; j++) {
     if (second[j] !== first[j]) {
       newArray.push(second[j]);
-      console.log(second[j])
     }
   }
   return newArray;
