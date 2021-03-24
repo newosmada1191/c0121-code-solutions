75f1fc0a63e9931c01d796193222f2bc4e752377
Owen Adams <owenadams.main@gmail.com>
2021-03-24 19:33:55 -0400

    completed react-class-components exercise

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function CustomButton(props) {
-//   return (
-//     <button>{ props.text }</button>
-//   );
-// }
-
 class CustomButton extends React.Component {
   render() {
     return <button>{ this.props.text }</button>;
