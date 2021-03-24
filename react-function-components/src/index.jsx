29a20675c3b6f9774a9d66ce1317fcb9034fb8d7
Owen Adams <owenadams.main@gmail.com>
2021-03-24 15:53:43 -0400

    completed react-function-components exercise

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Button() {
+  return <button>Click Me!</button>;
+}
+
+ReactDOM.render(
+  <Button />,
+  document.getElementById('root')
+);
