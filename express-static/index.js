03bac33e2169a0c62112946107fa2ecaf1834dee
Owen Adams <owenadams.main@gmail.com>
2021-03-29 14:16:18 -0400

    removed lines 6-8 in index.js

--- express-static/index.js
+++ express-static/index.js
@@ -3,10 +3,6 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
-app.get('/index.html', (req, res, next) => {
-  res.sendStatus(200);
-});
-
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
