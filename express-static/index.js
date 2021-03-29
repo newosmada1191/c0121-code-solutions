a1a1d569c8777e920145559328efe5bbce9cbad7
Owen Adams <owenadams.main@gmail.com>
2021-03-29 14:30:41 -0400

    assigned path.join and express.static functions to variables

--- express-static/index.js
+++ express-static/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const port = 3000;
 const app = express();
+const publicPath = path.join(__dirname, 'public');
+const staticMiddleware = express.static(publicPath);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(staticMiddleware);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
