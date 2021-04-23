3f45d6efcc903dc2d233736d2df3c6c12fcc8ab0
Owen Adams <owenadams.main@gmail.com>
2021-04-22 21:46:11 -0400

    Completed reverseWords(string) function

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,13 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  const stringArray = string.split(' ');
+  let newString = '';
+  for (let i = 0; i < stringArray.length; i++) {
+    let reverseString = '';
+    for (let j = stringArray[i].length - 1; j >= 0; j--) {
+      reverseString += stringArray[i][j];
+    }
+    newString += reverseString + ' ';
+  }
+  return newString.trim();
+}
