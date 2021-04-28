783dd09310ae34f39da174be9df27205083217ca
Owen Adams <owenadams.main@gmail.com>
2021-04-28 15:52:56 -0400

    Created an object with 4 unqiue players with a score value and a hand value

---
+++ working-with-collections/main.js
@@ -0,0 +1,26 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+const players = [
+  {
+    player: 'Arthas',
+    hand: [],
+    score: 0
+  },
+  {
+    player: 'Vegeta',
+    hand: [],
+    score: 0
+  },
+  {
+    player: 'Elliot',
+    hand: [],
+    score: 0
+  },
+  {
+    player: 'Saul Goodman',
+    hand: [],
+    score: 0
+  }
+];
+
+console.log(players);
