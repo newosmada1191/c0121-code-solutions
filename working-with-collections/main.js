4a307b5b5bd5b6a268a0939881443439d2138349
Owen Adams <owenadams.main@gmail.com>
2021-05-04 12:49:15 -0400

    Created two functions to create a deck object

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -23,4 +23,25 @@ const players = [
   }
 ];
 
+function card(value, name, suit) {
+  this.value = value;
+  this.name = name;
+  this.suit = suit;
+}
+
+function deck() {
+  this.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  this.suits = ['Clubs', 'Diamonds', 'Hears', 'Spades'];
+  const cards = [];
+  for (let i = 0; i < this.suits.length; i++) {
+    for (let j = 0; j < this.names.length; j++) {
+      // eslint-disable-next-line new-cap
+      cards.push(new card(j + 1, this.names[j], this.suits[i]));
+    }
+  }
+  console.log('Value of cards:', cards);
+  return cards;
+}
+
+deck();
 console.log(players);
