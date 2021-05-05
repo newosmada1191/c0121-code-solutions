b993b4eef8c7eaa81c350309c4b0de4c3cd3637f
Owen Adams <owenadams.main@gmail.com>
2021-05-05 03:14:38 -0400

    Created playerScore and Players function

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -1,52 +1,40 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-function Card(value, name, suit) {
+function Card(value, score, suit) {
   this.value = value;
-  this.name = name;
+  this.score = score;
   this.suit = suit;
 }
 
 function deck() {
-  this.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  this.score = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   this.suits = ['Clubs', 'Diamonds', 'Hears', 'Spades'];
   const cards = [];
   for (let i = 0; i < this.suits.length; i++) {
-    for (let j = 0; j < this.names.length; j++) {
-      cards.push(new Card(j + 2, this.names[j], this.suits[i]));
+    for (let j = 0; j < this.score.length; j++) {
+      cards.push(new Card(j + 2, this.score[j], this.suits[i]));
     }
   }
   return cards;
 }
 
-// function player(name, hand, score) {
-//   this.name = name;
-//   this.hand = hand;
-//   this.score = score;
-// }
-
-const players = {
-  Arthas: {
-    hand: [],
-    score: 0
-  },
-  Elliot: {
-    hand: [],
-    score: 0
-  },
-  SaulGoodman: {
-    hand: [],
-    score: 0
-  },
-  Vegeta: {
-    hand: [],
-    score: 0
-  }
-};
+function Players(name, hand, score) {
+  this.name = name;
+  this.hand = hand;
+  this.score = score;
+}
 
 function playerScore() {
   const shuffledDeck = _.shuffle(deck());
   console.log(shuffledDeck);
+  console.log(shuffledDeck.length);
+  this.names = ['Arthas', 'Elliot Alderson', 'Saul Goodman', 'Vegeta'];
+  this.hand = [];
+  const hands = [];
+  for (let k = 0; k < this.names.length; k++) {
+    hands.push(new Players(this.names[k], 'test', 'test'));
+  }
+  return hands;
 }
 
-console.log(players);
 console.log(playerScore());
