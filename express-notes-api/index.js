17d711efe7d54da049887b05b97c3b1e586b62ed
Owen Adams <owenadams.main@gmail.com>
2021-05-11 10:02:41 -0400

    Created arrow function to listen on port 3000, added a get method to view the package.json

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -5,8 +5,19 @@ const app = express();
 // eslint-disable-next-line no-console
 console.log(express);
 // eslint-disable-next-line no-console
-console.log(data);
-// eslint-disable-next-line no-console
 console.log(fs);
 // eslint-disable-next-line no-console
 console.log(app);
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is listening on port 3000!');
+});
+
+app.get('/api/notes', (req, res) => {
+  const jsonArray = [];
+  for (const key in data.notes) {
+    jsonArray.push(data.notes[key]);
+  }
+  res.status(200).json(jsonArray);
+});
