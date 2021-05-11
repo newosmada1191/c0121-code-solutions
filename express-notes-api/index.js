23587fb54b2980a73aaca44cf00b87c8a9aa61a4
Owen Adams <owenadams.main@gmail.com>
2021-05-11 09:38:16 -0400

    Added data, fs, and app variables

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const data = require('./data.json');
+const fs = require('fs');
+const app = express();
 // eslint-disable-next-line no-console
 console.log(express);
+// eslint-disable-next-line no-console
+console.log(data);
+// eslint-disable-next-line no-console
+console.log(fs);
+// eslint-disable-next-line no-console
+console.log(app);
