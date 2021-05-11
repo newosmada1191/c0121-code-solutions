59b9c272efb6a0a3e78887e3c37bb94288959869
Owen Adams <owenadams.main@gmail.com>
2021-05-11 10:09:39 -0400

    repush

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,8 +3,6 @@ const data = require('./data.json');
 const fs = require('fs');
 const app = express();
 // eslint-disable-next-line no-console
-console.log(express);
-// eslint-disable-next-line no-console
 console.log(fs);
 
 app.listen(3000, () => {
