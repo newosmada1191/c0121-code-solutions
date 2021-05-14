cd6f9d541d118431a07593c7331b9226bfa4971a
Owen Adams <owenadams.main@gmail.com>
2021-05-14 14:25:08 -0400

    Added db variable

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();
 // eslint-disable-next-line no-console
 console.log(app);
-// eslint-disable-next-line no-console
-console.log(pg);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Server is listening on port 3000!');
 });
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// eslint-disable-next-line no-console
+console.log(db);
