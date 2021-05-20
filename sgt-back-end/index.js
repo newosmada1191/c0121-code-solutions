e582845400a4bc7592683dc5ab3efd53fa70e2db
Owen Adams <owenadams.main@gmail.com>
2021-05-19 23:02:17 -0400

    Removed app console.log, added app.get method to display all student grades

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,8 +1,6 @@
 const pg = require('pg');
 const express = require('express');
 const app = express();
-// eslint-disable-next-line no-console
-console.log(app);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -14,6 +12,21 @@ const db = new pg.Pool({
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+  select *
+    from "grades"
+    `;
+  db.query(sql)
+    .then(success => {
+      res.status(200).json(success.rows);
+    })
+    .catch(error => {
+      console.error(`error ${error}`);
+      res.sendStatus(500);
+    });
 });
 
 // eslint-disable-next-line no-console
